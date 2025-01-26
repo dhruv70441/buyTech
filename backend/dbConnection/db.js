8ebb284e894b540b0a1912ed8e4c086cac55758d
Dhruv Parmar <dhruvparmar70441@gmail.com>
2025-01-26 23:58:11 +0530

    created db.js and connected with mognodb

---
+++ backend/dbConnection/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        const url = `${process.env.MONGO_URL}/${process.env.DB_NAME}`
+        console.log(url);
+        
+        const conn = await mongoose.connect(url)
+        console.log("MongoDB Connected");
+    } catch (error) {
+        console.log(`Error: ${error.message}`);    
+        process.exit(1)    
+    }
+}
+
+export default connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import { configDotenv } from "dotenv";
 import express from "express";
-
+import connectDB from "./dbConnection/db.js";
+import mongoose from "mongoose";
 
 configDotenv();
-
+connectDB()
 const app = express();
+
 
 app.listen(process.env.PORT || 5000, () => {
     console.log(`server is running on port ${process.env.PORT}`);
