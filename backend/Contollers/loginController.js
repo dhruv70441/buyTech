bf1088ea4c5cc26d01048f5078f7784f93b7c16d
Dhruv Parmar <dhruvparmar70441@gmail.com>
2025-01-28 01:05:49 +0530

    created login controller

--- backend/Contollers/loginController.js
+++ backend/Contollers/loginController.js
@@ -1,4 +1,40 @@
-const loginController = (req,res) => {
-    res.send('login')
+import User from "../models/user.model.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"
+
+const loginController = async (req,res) => {
+    const { email, password } = req.body
+    try {
+        const user = await User.findOne({email})
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: "Invalid email or password."
+            })
+        }
+    
+        const isValidPassword = await bcrypt.compare(password, user.password);
+        if (!isValidPassword) {
+            return res.status(404).json({
+                success: false,
+                message: "Invalid email or password."
+            })   
+        }
+        
+        const token = jwt.sign({_id: user._id}, process.env.SECRET, {expiresIn:'90d'})
+
+        return res.status(200).json({
+            status: true,
+            message: "user login successfully.",
+            token
+        })    
+
+    } catch (err) {
+        return res.status(500).json({
+            success: false,
+            message: "Error in login",
+            error: err.message
+        })
+    }
 }
 export default loginController;
--- backend/Contollers/signupController.js
+++ backend/Contollers/signupController.js
@@ -1,5 +1,38 @@
-const signupController = (req, res) => {
-    res.send('sign up')
+import User from "../models/user.model.js";
+import bcrypt from "bcrypt"
+
+const signupController = async (req, res) => {
+    
+    const { firstName, lastName, email, password } = req.body;
+    try {
+        const userExist = await User.findOne({email})
+        if(userExist){
+            return res.status(409).json({
+                success: false,
+                message: 'User already exist. Please login!'
+            })
+        }
+    
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
+    
+        const user = new User({firstName,lastName,email,password: hashedPassword})
+        await user.save()
+        
+        return res.status(201).json({
+            success:true,
+            message:'User created successfully',
+            user
+        })
+        
+    } catch (err) {
+        return res.status(500).json({
+            success:false,
+            message:'Error in signup',
+            error: err.message
+        })        
+    }
+    
 }
 
 export default signupController;
