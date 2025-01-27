b85a6b486133d5532e37ecaae195b9a5eec6a3e5
Dhruv Parmar <dhruvparmar70441@gmail.com>
2025-01-27 21:37:37 +0530

    created Footer component

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer >
+        <div>
+            All rights reserved &copy; buyTech.
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
