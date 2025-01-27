7c477c5f434187322716e1272f55ceae304efdff
Dhruv Parmar <dhruvparmar70441@gmail.com>
2025-01-27 21:33:02 +0530

    created pages and component files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Notfound from './pages/Notfound';
+import Dashboard from './pages/Dashboard';
 function App() {
   return(
-    <div className='text-4xl'>
-      APP
-    </div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/signup' element={<Signup />} />
+      <Route path='/dashboard' element={<Dashboard />} />
+      <Route path='*' element={<Notfound />} />
+    </Routes>
+    
   )  
 }
 
