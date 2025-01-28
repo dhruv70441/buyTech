5788fb15cc1cd1767b280706b460cc5d22d424ce
Dhruv Parmar <dhruvparmar70441@gmail.com>
2025-01-28 16:09:01 +0530

    created login and signup pages

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Layout from '../components/Layout'
 
 const Login = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] =  useState("");
+
   return (
     <Layout>
-      <div>
-        Login
+      <div className=' flex flex-col items-center mt-30'>
+        <h3>Login</h3>
+        <form 
+          className=' flex flex-col '
+        >
+          <label>Email</label>
+          <input 
+            type="email" 
+            className=' border-2' 
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label>Password</label>
+          <input 
+            type="password" 
+            className=' border-2' 
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type='submit' className=' border-2'>Login</button>
+        </form>
       </div>
     </Layout>
-    
   )
 }
-
 export default Login
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,15 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Layout from '../components/Layout'
 
 const Signup = () => {
+
+  const [firstName, setFirstName] = useState("")
+  const [lastName, setLastName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  
   return (
     <Layout>
-      <div>
-        Signup
+      <div className=' flex flex-col items-center mt-30'>
+        <h3>Sign Up</h3>
+        <form 
+          className=' flex flex-col '
+        >
+          <label>First Name</label>
+          <input type="text" className=' border-2' />
+          <label>Last Name</label>
+          <input type="text" className=' border-2' />
+          <label>Email</label>
+          <input type="email" className=' border-2' />
+          <label>Password</label>
+          <input type="text" className=' border-2' />
+          <button type='submit' className=' border-2'>signup</button>
+        </form>
       </div>
     </Layout>
-    
   )
 }
-
 export default Signup;
