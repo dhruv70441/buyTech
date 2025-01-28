fa414bec0f373c3871c1f3a3800fd4e8a247b345
Dhruv Parmar <dhruvparmar70441@gmail.com>
2025-01-28 16:16:28 +0530

    added shop route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,12 @@ import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Notfound from './pages/Notfound';
 import Dashboard from './pages/Dashboard';
+import Shop from './pages/Shop';
 function App() {
   return(
     <Routes>
       <Route path='/' element={<Home />} />
+      <Route path='/shop' element={<Shop />} />
       <Route path='/login' element={<Login />} />
       <Route path='/signup' element={<Signup />} />
       <Route path='/dashboard' element={<Dashboard />} />
